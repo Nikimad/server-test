2a5084949644c035e668dc0fcc7cf3155633ae36
Nikimad <PybePoiD@gmail.com>
2023-06-21 13:13:10 +0300

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+#  Boards DB
+
+[Project link](https://github.com/Nikimad/Boards)
+
+[snp internship 2022](https://snp.agency/en)
---
+++ api/server.js
@@ -0,0 +1,9 @@
+import jsonServer from "json-server";
+import fs from "fs";
+
+export const server = jsonServer.create();
+const db = JSON.parse(fs.readFileSync("db.json"));
+const router = jsonServer.router(db);
+
+server.use(router);
+server.listen(3000);
