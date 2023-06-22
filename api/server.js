54aad0b7b724cea2e71cbcbb41df1c8df58a622d
Nikimad <PybePoiD@gmail.com>
2023-06-22 10:38:19 +0300

    Change

--- api/server.js
+++ api/server.js
@@ -5,5 +5,10 @@ export const server = jsonServer.create();
 const db = JSON.parse(fs.readFileSync("db.json"));
 const router = jsonServer.router(db);
 
+server.post("/boards", (req, res, next) => {
+  db.boards.ids = [req.body.id, ...db.boards.ids];
+  next();
+});
+
 server.use(router);
 server.listen(3000);
