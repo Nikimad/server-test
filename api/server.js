5bb688e2b19884997263d3f8724483b5106487ca
Nikimad <PybePoiD@gmail.com>
2023-06-22 14:59:26 +0300

    change

--- api/server.js
+++ api/server.js
@@ -4,8 +4,5 @@ import fs from "fs";
 export const server = jsonServer.create();
 const db = JSON.parse(fs.readFileSync("db.json"));
 const router = jsonServer.router(db);
-
-server.use(jsonServer.bodyParser);
-
 server.use(router);
 server.listen(3000);
